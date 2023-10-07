9bbbe4395dff3a1501c11a45ed1b48dd2a40f189
Vijay Venugopal Menon <vijayvmenon@gmail.com>
2023-10-06 21:33:08 -0500

    updated env configs

--- app/config/mongodb.js
+++ app/config/mongodb.js
@@ -1,27 +1,27 @@
 // Loads a reusable Mongo client for the application.
-const MongoClient = require('mongodb').MongoClient
-const dbConfig = require('./db.config.js')
+const MongoClient = require("mongodb").MongoClient;
+const dbConfig = require("./db.config.js");
 
 // TODO: Check that your config file is exporting out an object with properties used in the next line.
-const client = new MongoClient(dbConfig.url)
+const client = new MongoClient(process.env.DATABASE_URL);
 
 client
   .connect()
   .then(() => {
-    console.info('MongoDB Client connected and running')
+    console.info("MongoDB Client connected and running");
   })
   .catch((err) => {
-    console.error('Error starting MongoDB Client', err.message)
+    console.error("Error starting MongoDB Client", err.message);
 
     // Exit process with failure
-    process.exit(1)
-  })
+    process.exit(1);
+  });
 
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   client.close().then(() => {
-    console.info('MongoDB Client disconnected')
-    process.exit(0)
-  })
-})
+    console.info("MongoDB Client disconnected");
+    process.exit(0);
+  });
+});
 
-module.exports = client
+module.exports = client;
